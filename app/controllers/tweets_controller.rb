6d0ca4cbe7309a70af9e37a942be2928e7f5e99e
Kenia Valladarez <keniavalladarez@gmail.com>
2016-12-22 08:09:27 -0500

    Completed explorer mode

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,7 +3,7 @@ class TweetsController < ApplicationController
 
   # GET /tweets
   def index
-    @tweets = Tweet.all.order("created_at DESC")
+    @tweets = Tweet.all.order("created_at DESC").where("text LIKE ?", "%#{params[:search]}%")
   end
 
   # GET /tweets/1
